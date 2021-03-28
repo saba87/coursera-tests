fff4eb4ba038150830e035364f3e24b88f9249d9
Saba <80032085+saba87@users.noreply.github.com>
2021-03-28 13:16:50 +0430

    Add files via upload

---
+++ module4-solution/speak.js
@@ -0,0 +1,12 @@
+ var byeSpeaker ={
+    speak :function (name){
+
+      console.log("Good bye "+ name);
+    }
+};
+ var helloSpeaker ={
+    speak :function (name){
+
+      console.log("Hello  "+ name);
+    }
+};
